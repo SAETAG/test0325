84ad007c534f860c0934a8ad8f49c0607feaa3ff
SAETAG <saeko.96@gmail.com>
2025-03-25 16:10:35 +0000

    クライアントサイドでの環境変数検証をスキップ

--- env/server.js
+++ env/server.js
@@ -13,15 +13,12 @@ if (!_serverEnv.success) {
         JSON.stringify(_serverEnv.error.format(), null, 4)
     );
 
-    
     // ブラウザ環境では終了せず、サーバーサイドのみ終了する
     if (typeof window === 'undefined') {
         process.exit(1);
     }
-    process.exit(1);
+    return { ...clientEnv }; // クライアントサイドではクライアント環境変数のみを返す
 }
 
 // クライアント側用に定義した値も使用できるようマージしてエクスポート
-module.exports.serverEnv = _serverEnv.success 
-    ? { ..._serverEnv.data, ...clientEnv }
-    : { ...clientEnv }; // 失敗した場合もクライアント環境変数だけは返す
+module.exports.serverEnv = { ..._serverEnv.data, ...clientEnv }
